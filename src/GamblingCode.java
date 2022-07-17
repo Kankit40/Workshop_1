2fd92927db2a5bc96804e5676f10b88bd972fee8
Kankit40 <Kankit40@gmail.com>
2022-07-17 19:46:33 +0530

    UC2 code

---
+++ src/GamblingCode.java
@@ -0,0 +1,18 @@
+public class GamblingCode {
+    public static void main(String[] args) {
+        final int totalamount = 100;
+
+        int outcome = (int) (Math.random() * 2);
+        int temp = 0;
+
+        if (outcome == 1){
+            System.out.println("You won the bet");
+           temp = outcome + 1;
+            System.out.println("Total amount is " + temp);
+        }else {
+            System.out.println("You have lost the game");
+            temp = outcome - 1;
+            System.out.println("Total amount is " + temp);
+        }
+    }
+}
