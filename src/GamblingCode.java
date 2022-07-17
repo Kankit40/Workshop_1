f17e24f55ca50af252d6459444210117deed2c5c
Kankit40 <Kankit40@gmail.com>
2022-07-17 21:14:48 +0530

    Update

--- src/GamblingCode.java
+++ src/GamblingCode.java
@@ -4,19 +4,29 @@ public class GamblingCode {
 
         int outcome = (int) (Math.random() * 2);
         int temp = 0;
-        int total =0;
+        int total = 0;
+        int min_value = 50;
+        int max_value = 150;
+        int bet = 0;
 
-        while (int i = 0; i < 21; i++){
-        if (outcome == 1){
-            System.out.println("You won the bet");
-           temp = outcome + 1;
-            System.out.println("Total amount is " + temp);
-        }else {
-            System.out.println("You have lost the game");
-            temp = outcome - 1;
-            System.out.println("Total amount is " + temp);
-        }
-             total = temp + total;
-            System.out.println("Total amount will be" + total);
+        for (int i = 0; i < 21; i++) {
+
+            while (bet < max_value && bet > min_value) {
+                bet++;
+                if (outcome == 1) {
+                    System.out.println("You won the bet");
+                    temp = totalamount + 1;
+                    System.out.println("Total amount is " + temp);
+                } else {
+                    System.out.println("You have lost the game");
+                    temp = totalamount - 1;
+                    System.out.println("Total amount is " + temp);
+                }
+                total = temp + total;
+                System.out.println("Total amount will be" + total);
+            }
+        } int monthlyoutcome = total;
+        System.out.println(" The monthly amount is " + monthlyoutcome);
+
     }
 }
